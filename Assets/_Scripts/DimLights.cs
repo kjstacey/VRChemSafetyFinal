caa2fc69801ccbc67302ed1e8e696f01256f7a09
willcaller <130016894+willcaller@users.noreply.github.com>
2025-03-31 17:23:41 -0500

    fixed some stuff and things

--- Assets/_Scripts/DimLights.cs
+++ Assets/_Scripts/DimLights.cs
@@ -10,16 +10,19 @@ public class DimLights : MonoBehaviour
 {
     [SerializeField] Light light1;
     [SerializeField] Light light2;
+    [SerializeField] UnityEngine.UI.Button replayButton;
 
     // Start is called before the first frame update
     public void LowerIntensity()
     {
+        replayButton.gameObject.SetActive(false);
         light1.intensity = 0.5f;
         light2.intensity = 0.3f;
     }
 
     private void OnVideoPlaybackComplete(VideoPlayer vp)
     {
+        replayButton.gameObject.SetActive(false);
         StartCoroutine(Wait());
         light1.intensity = 1.07f;
         light2.intensity = 1.07f;
